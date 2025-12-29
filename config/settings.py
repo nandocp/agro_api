cf290f65c64c028d85b8435e654cc9e96fd63809
Fernando Caçador <fernando.cacador@proton.me>
2025-12-29 09:22:56 -0300

    Configured environment settings

---
+++ config/settings.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from pydantic import PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env', env_file_encoding='utf-8'
+    )
+
+    # App stuff
+    FASTAPI_ENV: str
+
+    # API
+    API_PATH: str
+    PROJECT_NAME: str = 'AgroAPI'
+    VERSION: str = '1.0.0'
+
+    # Database
+    DATABASE_URL: PostgresDsn
+
+    # Security
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    # CORS
+    CORS_ORIGINS: List[str]
+
+
+settings = Settings()
