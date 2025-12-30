36d24e88ffdaf32e69b1d37240b704e4fe79788a
Fernando Caçador <fernando.cacador@proton.me>
2025-12-30 18:10:32 -0300

    Created user repository for data access

---
+++ agro_api/repositories/user.py
@@ -0,0 +1,30 @@
+from http import HTTPStatus
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from agro_api.entities.user import User
+
+
+class UserRepository:
+    def __init__(self, session=None):
+        self.session: Session | None = session
+
+    def create_user(self, schema_params) -> User:
+        db_user = self.session.scalar(
+            select(User).where(User.email == schema_params.email)
+        )
+
+        if db_user:
+            return {
+                'status_code': HTTPStatus.CONFLICT,
+                'detail': 'User already exists'
+            }
+
+        new_user = User(**schema_params.model_dump())
+
+        self.session.add(new_user)
+        self.session.commit()
+        self.session.refresh(new_user)
+
+        return new_user
