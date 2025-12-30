c1d2035c216b6c61de1d42e3d11f811664faf2f5
Fernando Caçador <fernando.cacador@proton.me>
2025-12-30 18:10:16 -0300

    Created users router

---
+++ agro_api/routers/users.py
@@ -0,0 +1,37 @@
+from http import HTTPStatus
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+
+# from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+# from agro_api.entities.user import User
+from agro_api.schemas.user import UserPostPayloadSchema, UserPostResponseSchema
+from agro_api.services.user import UserService
+from config.database import get_session
+
+router = APIRouter(prefix='/users', tags=['users'])
+
+session = Annotated[Session, Depends(get_session)]
+
+
+@router.post(
+    '/', status_code=HTTPStatus.CREATED, response_model=UserPostResponseSchema
+)
+def create(user: UserPostPayloadSchema, session: session):
+    response = UserService(session).create(user)
+
+    if isinstance(response, dict):
+        raise HTTPException(**response)
+
+    return response
+
+
+# def show(user_id: str, session: Session=Depends(get_session)):
+
+# def index():
+
+# def update():
+
+# def delete():
