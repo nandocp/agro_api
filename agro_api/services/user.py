62db5bd76f504e5258ea81cc83ed5b9069401fc4
Fernando Caçador <fernando.cacador@proton.me>
2025-12-30 18:10:04 -0300

    Created user service

---
+++ agro_api/services/user.py
@@ -0,0 +1,27 @@
+from agro_api.entities.user import User
+from agro_api.repositories.user import UserRepository
+from config.security import hash_password
+
+from .base import BaseService
+
+
+class UserService(BaseService):
+    def __init__(self, session=None):
+        super().__init__(User)
+        self.repository = UserRepository(session)
+
+    def create(self, schema_params):
+        schema_params.password = hash_password(schema_params.password)
+        return self.repository.create_user(schema_params)
+
+    def get_one(self, id: int):
+        pass
+
+    def get_many(self, *, skip: int = 0, limit: int = 100):
+        pass
+
+    def update(self, *, db_obj, obj_in):
+        pass
+
+    def remove(self, *, id: int):
+        pass
