adb943eaa4b01e8c29c886d695816fde3b40e3d1
Fernando Caçador <fernando.cacador@proton.me>
2025-12-30 18:04:47 -0300

    Updated service

--- agro_api/services/base.py
+++ agro_api/services/base.py
@@ -1,40 +1,37 @@
-# from abc import ABC, abstractmethod
-# from typing import Generic, List, Optional, TypeVar
+from abc import ABC, abstractmethod
+from typing import Generic, List, Optional, TypeVar
 
-# from pydantic import BaseModel
-# from sqlalchemy.orm import Session
+from pydantic import BaseModel
 
-# ModelType = TypeVar('ModelType')
-# CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
-# UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
+ModelType = TypeVar('ModelType')
+CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
+UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
 
 
-# class ServiceBase(
-# ABC, Generic[ModelType, CreateSchemaType, UpdateSchemaType]
-# ):
-#     def __init__(self, model: ModelType):
-#         self.model = model
+class BaseService(ABC, Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
+    def __init__(self, model: ModelType) -> None:
+        self.model: ModelType = model
 
-#     @abstractmethod
-#     def get_one(self, db: Session, id: int) -> Optional[ModelType]:
-#         pass
+    @abstractmethod
+    def get_one(self, id: int) -> Optional[ModelType]:
+        pass
 
-#     @abstractmethod
-#     def get_many(
-#         self, db: Session, *, skip: int = 0, limit: int = 100
-#     ) -> List[ModelType]:
-#         pass
+    @abstractmethod
+    def get_many(
+        self, *, offset: int = 0, limit: int = 100
+    ) -> List[ModelType]:
+        pass
 
-#     @abstractmethod
-#     def create(self, db: Session, *, obj_in: CreateSchemaType) -> ModelType:
-#         pass
+    @abstractmethod
+    def create(self, *, obj_in: CreateSchemaType) -> ModelType:
+        pass
 
-#     @abstractmethod
-#     def update(
-#         self, db: Session, *, db_obj: ModelType, obj_in: UpdateSchemaType
-#     ) -> ModelType:
-#         pass
+    @abstractmethod
+    def update(
+        self, *, db_obj: ModelType, obj_in: UpdateSchemaType
+    ) -> ModelType:
+        pass
 
-#     @abstractmethod
-#     def remove(self, db: Session, *, id: int) -> ModelType:
-#         pass
+    @abstractmethod
+    def remove(self, *, id: int) -> ModelType:
+        pass
