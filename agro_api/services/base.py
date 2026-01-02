7da87725208fb495412c4303d497f12d198faa21
Fernando Caçador <fernando.cacador@proton.me>
2026-01-02 11:57:16 -0300

    New service #session attribute

--- agro_api/services/base.py
+++ agro_api/services/base.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Generic, List, Optional, TypeVar
 
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 
 ModelType = TypeVar('ModelType')
 CreateSchemaType = TypeVar('CreateSchemaType', bound=BaseModel)
@@ -9,8 +10,9 @@ UpdateSchemaType = TypeVar('UpdateSchemaType', bound=BaseModel)
 
 
 class BaseService(ABC, Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: ModelType) -> None:
+    def __init__(self, model: ModelType, session: None | Session) -> None:
         self.model: ModelType = model
+        self.session = session
 
     @abstractmethod
     def get_one(self, id: int) -> Optional[ModelType]:
