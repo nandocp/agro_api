90d89b2a557429ae544869ef1c42d5ae23a6f7c1
Fernando Caçador <fernando.cacador@proton.me>
2026-01-01 21:05:05 -0300

    Updated jwt payload

--- config/jwt.py
+++ config/jwt.py
@@ -1,23 +1,34 @@
 from datetime import datetime, timedelta
+from uuid import uuid6
 from zoneinfo import ZoneInfo
 
-from jwt import encode
+from jwt import decode, encode
 
 from config.settings import settings
 
 
 def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
+    payload = data.copy()
     expire = set_expiration()
-    to_encode.update({'exp': expire})
-    return encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
-    )
+    jti = generate_jti()
+    payload.update({'exp': expire, 'jti': str(jti)})
+    return {
+        'jti': jti,
+        'jwt': encode(
+            payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+        ),
+    }
+
 
 def set_expiration() -> int:
     return datetime.now(tz=ZoneInfo('UTC')) + timedelta(
         minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
     )
 
+
 def generate_jti():
-    return
+    return uuid6()
+
+
+def decode_access_token(jwt: str) -> dict:
+    return decode(jwt, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
