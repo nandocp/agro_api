faa859b221a1c3e96a3e312123608c1f3a5985b2
Fernando Caçador <fernando.cacador@proton.me>
2026-01-02 16:53:52 -0300

    Create index and show router methods and its tests

--- agro_api/routers/estates.py
+++ agro_api/routers/estates.py
@@ -1,10 +1,14 @@
 from http import HTTPStatus
+from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from agro_api.schemas.estate import (
+    EstateFilter,
+    EstateGetResponseSchema,
     EstatePostPayloadSchema,
     EstatePostResponseSchema,
+    EstatesList,
 )
 from agro_api.services.estate import EstateService
 from config.database import session
@@ -13,8 +17,7 @@ from config.user import current_user
 router = APIRouter(prefix='/estates', tags=['estates'])
 
 
-@router.post(
-    '/',
+@router.post('/',
     response_model=EstatePostResponseSchema,
     status_code=HTTPStatus.CREATED
 )
@@ -24,3 +27,17 @@ def create(
     estate: EstatePostPayloadSchema
 ):
     return EstateService(session).create(estate, current_user.id)
+
+
+@router.get('/', response_model=EstatesList, status_code=HTTPStatus.OK)
+def index(
+    session: session,
+    current_user: current_user,
+    filters: Annotated[EstateFilter, Query()]
+):
+    return EstateService(session).get_many(current_user.id, filters)
+
+
+@router.get('/{estate_id}', response_model=EstateGetResponseSchema)
+def show(session: session, current_user: current_user, estate_id: str):
+    return EstateService(session).get_one(current_user.id, estate_id)
--- tests/routers/test_estates.py
+++ tests/routers/test_estates.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from agro_api.entities.estate import EstateKind
 from tests.factories.estates import EstateFactory
 
 
@@ -25,3 +26,137 @@ def test_create_estate(client, session, user, token):
     #     'id': str(user_db.id),
     #     'created_at': str(user_db.created_at).replace(' ', 'T'),
     # }
+
+
+def test_get_index_estates_without_query(
+    client, token, user, session, other_user
+):
+    batch_size = 3
+    estates = EstateFactory.create_batch(batch_size, user_id=user.id)
+    session.add_all(estates)
+
+    other_estates = EstateFactory.create_batch(
+        batch_size, user_id=other_user.id
+    )
+    session.add_all(other_estates)
+    session.commit()
+
+    response = client.get(
+        '/estates',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()['estates']) == batch_size
+
+
+def test_get_index_estates_with_kind_query(
+    client, token, user, session, other_user
+):
+    batch_size = 3
+    rural_estates = EstateFactory.create_batch(
+        batch_size, user_id=user.id, kind=EstateKind.rural
+    )
+    urban_estates = EstateFactory.create_batch(
+        batch_size, user_id=user.id, kind=EstateKind.periurban
+    )
+    session.add_all(rural_estates)
+    session.add_all(urban_estates)
+
+    other_estates = EstateFactory.create_batch(
+        batch_size, user_id=other_user.id
+    )
+    session.add_all(other_estates)
+    session.commit()
+
+    response = client.get(
+        '/estates/?kind=rural',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()['estates']) == batch_size
+
+
+def test_get_index_estates_with_slug_query(
+    client, token, user, session, other_user
+):
+    batch_size = 3
+    slug = EstateFactory().slug
+    slug_estate = EstateFactory.create(
+        user_id=user.id, slug=slug
+    )
+    other_estates = EstateFactory.create_batch(
+        batch_size, user_id=user.id, kind=EstateKind.periurban
+    )
+    session.add(slug_estate)
+    session.add_all(other_estates)
+
+    other_estates = EstateFactory.create_batch(
+        batch_size, user_id=other_user.id
+    )
+    session.add_all(other_estates)
+    session.commit()
+
+    response = client.get(
+        f'/estates/?slug={slug}',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()['estates']) == 1
+
+
+def test_get_index_estates_with_label_query(
+    client, token, user, session, other_user
+):
+    batch_size = 3
+    label = EstateFactory().label
+    slug_estate = EstateFactory.create_batch(
+        batch_size, user_id=user.id, label=label
+    )
+    other_estates = EstateFactory.create_batch(batch_size, user_id=user.id)
+    session.add_all(slug_estate)
+    session.add_all(other_estates)
+
+    other_estates = EstateFactory.create_batch(
+        batch_size, user_id=other_user.id
+    )
+    session.add_all(other_estates)
+    session.commit()
+
+    response = client.get(
+        f'/estates/?label={label}',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert len(response.json()['estates']) == batch_size
+
+
+def test_get_one_estate_with_client_token(
+    client, token, user, session
+):
+    estate = EstateFactory(user_id=user.id)
+    estates = EstateFactory.create_batch(3, user_id=user.id)
+    session.add(estate)
+    session.add_all(estates)
+    session.commit()
+
+    response = client.get(
+        f'/estates/{estate.id}',
+        headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'closed_at': None,
+        'created_at': str(estate.created_at).replace(' ', 'T'),
+        'id': str(estate.id),
+        'kind': estate.kind.value,
+        'label': estate.label,
+        'opened_at': str(estate.opened_at).replace(' ', 'T'),
+        'slug': estate.slug,
+        'updated_at': str(estate.updated_at).replace(' ', 'T'),
+        'user_id': str(estate.user_id)
+    }
