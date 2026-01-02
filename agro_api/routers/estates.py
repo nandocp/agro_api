4fe1886a774cb56b0d38ca5d4de240a531f28d8e
Fernando Caçador <fernando.cacador@proton.me>
2026-01-02 11:55:00 -0300

    Created #create

--- agro_api/routers/estates.py
+++ agro_api/routers/estates.py
@@ -1,10 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import APIRouter
 
-from agro_api.schemas.estate import EstatePostPayloadSchema
+from agro_api.schemas.estate import (
+    EstatePostPayloadSchema,
+    EstatePostResponseSchema,
+)
+from agro_api.services.estate import EstateService
+from config.database import session
+from config.user import current_user
 
 router = APIRouter(prefix='/estates', tags=['estates'])
 
 
-@router.post('/', response_model=EstatePostPayloadSchema)
-def create():
-    pass
+@router.post(
+    '/',
+    response_model=EstatePostResponseSchema,
+    status_code=HTTPStatus.CREATED
+)
+def create(
+    session: session,
+    current_user: current_user,
+    estate: EstatePostPayloadSchema
+):
+    return EstateService(session).create(estate, current_user.id)
