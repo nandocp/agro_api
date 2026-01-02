7d43d5b5d75a07deecf97ab654f549200ed6a519
Fernando Caçador <fernando.cacador@proton.me>
2026-01-01 21:07:29 -0300

    Removed unecessary commented out lines, added except to get_session() and started returning Annotated session

--- config/database.py
+++ config/database.py
@@ -1,6 +1,7 @@
-# from contextlib import contextmanager
-# from warnings import warn
+from typing import Annotated
+from warnings import warn
 
+from fastapi import Depends
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session
 
@@ -10,53 +11,17 @@ engine: Engine = create_engine(
     str(settings.DATABASE_URL), pool_pre_ping=True, max_overflow=64
 )
 
-# local_session: Session = sessionmaker(
-#     autocommit=False, autoflush=False, bind=engine
-# )
-
-
-# @contextmanager
-# def db_session():
-#     db = local_session()
-
-#     try:
-#         yield db
-#     except Exception as e:
-#         print(e)
-#         warn('DB operation failed. Auto-rollbacking...')
-#         db.rollback()
-#     finally:
-#         db.close()
-
 
 def get_session():
-    print('antes do with Session')
     with Session(engine) as session:
-        print('dentro do with Session')
-        print(session)
-        yield session
+        try:
+            yield session
+        except Exception as error:
+            print(error)
+            warn('DB operation failed. Auto-rollbacking...')
+            session.rollback()
+        finally:
+            session.close()
 
 
-# class DBConnection:
-#     def __init__(self) -> None:
-#         self.__engine = self.__create_engine()
-#         self.session = None
-
-#     @classmethod
-#     def __create_engine(self) -> Engine:
-#         return create_engine(str(settings.DATABASE_URL), pool_pre_ping=True)
-
-#     def get_engine(self) -> Engine:
-#         return self.__engine
-
-#     def __enter__(self):
-#         self.session = sessionmaker(
-#             autocommit=False, autoflush=False, bind=self.__engine
-#         )
-
-#         return self
-
-#     def __exit__(self, exc_type, exc_val, exc_tb):
-#         print(exc_type, exc_tb, exc_val)
-#         self.session.object_session().close()
-#         # self.session.close()
+session = Annotated[Session, Depends(get_session)]
