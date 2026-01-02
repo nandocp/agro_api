66377b1afd186fd17fd1adbbc3d177296600884e
Fernando Caçador <fernando.cacador@proton.me>
2026-01-02 16:53:04 -0300

    Schema add

--- agro_api/schemas/common.py
+++ agro_api/schemas/common.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+
+from pydantic import UUID4, BaseModel, Field
 
 
-class FilterPage():
+class FilterPage(BaseModel):
     offset: int = Field(ge=0, default=0)
     limit: int = Field(ge=0, default=10)
+
+
+class BaseSchema(BaseModel):
+    id: UUID4
+    created_at: datetime
+    updated_at: datetime
