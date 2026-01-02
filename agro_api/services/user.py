f402b28fedc114a21e74b7a794a23f00c1888610
Fernando Caçador <fernando.cacador@proton.me>
2026-01-02 12:01:30 -0300

    Removal of repository

--- agro_api/services/user.py
+++ agro_api/services/user.py
@@ -1,5 +1,6 @@
+from sqlalchemy import select
+
 from agro_api.entities.user import User
-from agro_api.repositories.user import UserRepository
 from config.password import hash_password
 
 from .base import BaseService
@@ -7,15 +8,25 @@ from .base import BaseService
 
 class UserService(BaseService):
     def __init__(self, session=None):
-        super().__init__(User)
-        self.repository = UserRepository(session)
+        super().__init__(User, session)
 
     def create(self, schema_params):
         schema_params.password = hash_password(schema_params.password)
-        return self.repository.create_user(schema_params)
+        db_user = self.find_by_email(schema_params.email)
+
+        if db_user:
+            return False
+
+        new_user = User(**schema_params.model_dump())
+
+        self.session.add(new_user)
+        self.session.commit()
+        self.session.refresh(new_user)
+
+        return new_user
 
     def get_one(self, id: str, jti: str):
-        user = self.repository.find_by_jti(jti)
+        user = self.find_by_jti(jti)
 
         if not user or str(user.id) != id:
             return False
@@ -25,8 +36,23 @@ class UserService(BaseService):
     def get_many(self, *, skip: int = 0, limit: int = 100):
         pass
 
-    def update(self, obj_id, obj_in):
-        return self.repository.update_user(obj_id, obj_in)
+    def update(self, user_id, params):
+        db_user = self.session.scalar(select(User).where(User.id == user_id))
+
+        if not db_user:
+            return False
+
+        db_user.name = params.name
+        self.session.commit()
+        self.session.refresh(db_user)
+
+        return db_user
 
     def remove(self, *, id: int):
         pass
+
+    def find_by_email(self, email) -> User:
+        return self.session.scalar(select(User).where(User.email == email))
+
+    def find_by_jti(self, jti) -> User | None:
+        return self.session.scalar(select(User).where(User.jti == jti))
