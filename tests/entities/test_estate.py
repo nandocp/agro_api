6a3ac0c1aa7796e2cd8f8a1bbda75ff3a690ab9f
Fernando Caçador <fernando.cacador@proton.me>
2026-01-06 09:03:34 -0300

    Addition of instance methods tests

--- tests/entities/test_estate.py
+++ tests/entities/test_estate.py
@@ -1,3 +1,5 @@
+import pytest
+
 from agro_api.entities.estate import EstateKind
 from tests.factories.estates import EstateFactory
 
@@ -18,3 +20,21 @@ def test_is_urban_false(session, user):
     estate = EstateFactory(user_id=user.id, kind=EstateKind.rural)
 
     assert not estate.is_urban()
+
+
+def test_area_with_limits(estate):
+    assert estate.area() == '0.04'
+
+
+@pytest.mark.asyncio
+async def test_area_without_limits(estate, session):
+    estate.limits = None
+    session.add(estate)
+    await session.commit()
+    await session.refresh(estate)
+
+    assert not estate.area()
+
+
+def test_custom__repr__(estate):
+    assert str(estate) == f'<Estate(slug={estate.slug}, area={estate.area()})>'
