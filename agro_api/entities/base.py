abc9328437cd11dbd10e9bd41133e26214624d6e
Fernando Caçador <fernando.cacador@proton.me>
2026-01-06 08:58:36 -0300

    Removal of unecessary lines

--- agro_api/entities/base.py
+++ agro_api/entities/base.py
@@ -1,9 +1,3 @@
 from sqlalchemy.orm import registry
 
 table_registry = registry()
-
-# from sqlalchemy.orm import DeclarativeBase
-
-# Define our base class
-# class Base(DeclarativeBase):
-#   pass
