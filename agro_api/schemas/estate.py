8d93da971ce7e22daed9d7d7b0dc102c2f49a5f8
Fernando Caçador <fernando.cacador@proton.me>
2026-01-06 09:00:04 -0300

    Addition of geographical columns in schemas

--- agro_api/schemas/estate.py
+++ agro_api/schemas/estate.py
@@ -1,16 +1,21 @@
 from datetime import datetime
+from typing import Any
 
-from pydantic import UUID4, BaseModel, ConfigDict, Field
+from pydantic import UUID4, BaseModel, ConfigDict, Field, validator
 
 from agro_api.entities.estate import EstateKind
 from agro_api.schemas.common import FilterPage
+from config.geometry import dump_geometry
 
 
 class EstateBase(BaseModel):
     label: str
     slug: str
     kind: EstateKind
-    model_config = ConfigDict(from_attrbutes=True)
+    model_config = ConfigDict(
+        arbitrary_types_allowed=True,
+        from_attrbutes=True,
+    )
 
 
 class EstateCreate(EstateBase):
@@ -20,9 +25,19 @@ class EstateCreate(EstateBase):
 class EstateItem(EstateCreate):
     id: UUID4
     user_id: UUID4
+    coordinates: Any
+    limits: Any
     created_at: datetime
     updated_at: datetime
     closed_at: datetime | None
+
+    _validate_limit = validator(
+        'limits', pre=True, allow_reuse=True
+    )(dump_geometry)
+
+    _validate_coordinates = validator(
+        'coordinates', pre=True, allow_reuse=True
+    )(dump_geometry)
 
 
 class EstatesList(BaseModel):
@@ -31,6 +46,16 @@ class EstatesList(BaseModel):
 
 class EstateUpdate(EstateCreate):
     closed_at: datetime | None
+    coordinates: Any  # Optional[Tuple[float, float]] = None
+    limits: Any  # Optional[List[Tuple[float, float]]] = None
+
+    # _validate_limit = validator(
+    #     'limits', pre=True, always=True, allow_reuse=True
+    # )(create_polygon_geometry)
+
+    # _validate_coordinates = validator(
+    #     'coordinates', pre=True, always=True, allow_reuse=True
+    # )(create_point_geometry)
 
 
 class EstateFilter(FilterPage):
