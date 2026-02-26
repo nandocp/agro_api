d047aa04a9f57d3567578d71df44ed1fa6c10b89
Fernando Caçador <fernando.cacador@proton.me>
2026-02-26 12:08:08 -0300

    Removed Plot related classes

--- agro_api/entities/estate/__init__.py
+++ agro_api/entities/estate/__init__.py
@@ -1,14 +1,3 @@
 from .estate import Estate, EstateKind
-from .plot import Plot
-from .plot_transition import PlotTransition, PlotTransitionType
-from .plot_protection import PlotProtection, ProtectionType
 
-__all__ = [
-    'Estate',
-    'EstateKind',
-    'Plot',
-    'PlotTransition',
-    'PlotTransitionType',
-    'PlotProtection',
-    'ProtectionType'
-]
+__all__ = ['Estate', 'EstateKind']
