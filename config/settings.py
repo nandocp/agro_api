97f90353dc37de1caaf51a0be790a574184b90d6
Fernando Caçador <fernando.cacador@proton.me>
2026-03-10 16:38:27 -0300

    Updated db addr

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
 
     # Database
     DATABASE_URL: str = (
-        'postgresql+psycopg://api_user:Agr0_420@db:5432/agro_db'
+        'postgresql+psycopg://postgres:postgres@db:5432/agro_db'
     )
 
     # Security
