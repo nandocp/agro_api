fdc6b42f2e3af94fc3fdc663cfe3f130cc495a80
Fernando Caçador <fernando.cacador@proton.me>
2026-03-13 11:46:03 -0300

    Added env db

--- scripts/db/drop.py
+++ scripts/db/drop.py
@@ -1,4 +1,5 @@
 import asyncio
+from sys import argv
 from urllib.parse import urlparse
 
 from sqlalchemy import text
@@ -11,6 +12,10 @@ async def drop_database() -> None:
     default_url = str(settings.DATABASE_URL).rsplit('/', 1)[0] + '/postgres'
     engine = create_async_engine(default_url, isolation_level='AUTOCOMMIT')
     db_name = urlparse(str(settings.DATABASE_URL)).path.lstrip('/')
+
+    ENV_SET = 2
+    if len(argv) == ENV_SET:
+        db_name = f'{db_name}_{argv[1]}'
 
     async with engine.connect() as conn:
         await conn.execute(
