2441ddf9877cc5ce42452d8cdfcdd64f0e7446f5
Fernando Caçador <fernando.cacador@proton.me>
2026-03-13 13:09:51 -0300

    Created new property

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     )
 
     # App stuff
-    FASTAPI_ENV: str = 'development'
+    ENVIRONMENT: str = 'development'
     DEBUG: bool | None = True
 
     # API
@@ -22,6 +22,9 @@ class Settings(BaseSettings):
         'postgresql+psycopg://postgres:postgres@127.0.0.1:5432/agro_db'
     )
 
+    TEST_DATABASE_URL: str = (
+        'postgresql+psycopg://postgres:postgres@127.0.0.1:5432/agro_db_test'
+    )
     # Security
     SECRET_KEY: str = (
         '076cf379f99ac223aead2e9e03e1b90466e03e8a3f0ec58e983d52589be70f22'
@@ -33,6 +36,12 @@ class Settings(BaseSettings):
     CORS_ORIGINS: List[str] = ['http://localhost:3000']
 
     LOG_LEVEL: str = 'DEBUG'
+
+    @property
+    def active_database_url(self) -> str:
+        if self.ENVIRONMENT == 'test':
+            return self.TEST_DATABASE_URL
+        return self.DATABASE_URL
 
 
 settings = Settings()
