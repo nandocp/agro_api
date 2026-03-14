35981548026bcca2f52a5c708432adfd83314f98
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 18:55:11 -0300

    Update token creation method

--- app/domain/accounts/auth.py
+++ app/domain/accounts/auth.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 
 from jwt import decode, encode
@@ -6,20 +5,15 @@ from jwt import decode, encode
 from config.settings import settings
 
 
-@dataclass(frozen=True)
-class TokenData:
-    jti: str
-    jwt: str
-
-
-def create_access_token(data: dict) -> TokenData:
+def create_access_token(sub: str, jti: str) -> str:
     now = datetime.now(timezone.utc)
-    payload = _sanitize_data(data)
-    payload.update({'exp': _expiration(now), 'iat': now})
-    return TokenData(
-        jti=payload['jti'],
-        jwt=encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM),
-    )
+    payload: dict = {
+        'exp': _expiration(now),
+        'iat': now,
+        'jti': str(jti),
+        'sub': str(sub),
+    }
+    return encode(payload, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
 def decode_access_token(token: str) -> dict:
@@ -28,9 +22,3 @@ def decode_access_token(token: str) -> dict:
 
 def _expiration(now) -> datetime:
     return now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-
-
-def _sanitize_data(data: dict) -> dict:
-    data['sub'] = str(data['sub'])
-    data['jti'] = str(data['jti'])
-    return data
