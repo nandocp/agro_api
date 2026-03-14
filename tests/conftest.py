c1fe56bf6662f3968c137f4db2e84d6205a3bf2a
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 14:03:13 -0300

    Fixes to #client, #session, #token

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
-# from secrets import token_hex
+from secrets import token_hex
+from uuid import uuid4
 
 import pytest
 import pytest_asyncio
-from fastapi.testclient import TestClient
+from httpx import ASGITransport, AsyncClient
 
 # from sqlalchemy import event
 from sqlalchemy.ext.asyncio import (
@@ -12,22 +13,25 @@ from sqlalchemy.ext.asyncio import (
 )
 from sqlalchemy.pool import StaticPool
 
+from app.domain.accounts.auth import create_access_token
 from app.main import app
 from app.shared.model.declarative import DeclarativeModel
-from config.database import get_session
+from config.database import get_session, get_session_with_commit
 from config.settings import settings
-from tests.factories.accounts import AccountFactory
+from tests.factories.accounts import AccountFactory, UserFactory
 from tests.factories.estates import EstateFactory
 
 
-@pytest.fixture
-def client(session):
-    def get_session_override():
-        return session
+@pytest_asyncio.fixture
+async def client(session):
+    async def get_session_override():
+        yield session
 
-    with TestClient(app) as client:
-        app.dependency_overrides[get_session] = get_session_override
-
+    app.dependency_overrides[get_session] = get_session_override
+    app.dependency_overrides[get_session_with_commit] = get_session_override
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url='http://localhost:8000'
+    ) as client:
         yield client
 
     app.dependency_overrides.clear()
@@ -44,13 +48,25 @@ async def session():
     async with engine.begin() as conn:
         await conn.run_sync(DeclarativeModel.metadata.create_all)
 
-    async with AsyncSession(engine, expire_on_commit=False) as session:
-        yield session
+    async with engine.connect() as conn:
+        await conn.begin_nested()  # savepoint
+        async with AsyncSession(bind=conn, expire_on_commit=False) as session:
+            yield session
+            await conn.rollback()  # rollback após cada teste
 
     async with engine.begin() as conn:
         await conn.run_sync(DeclarativeModel.metadata.drop_all)
 
     await engine.dispose()
+
+
+@pytest_asyncio.fixture
+async def persist(session):
+    async def _persist():
+        await session.commit()
+        session.expire_all()
+
+    return _persist
 
 
 @pytest_asyncio.fixture
@@ -63,14 +79,41 @@ async def account(session) -> AccountFactory:
     return await AccountFactory.create(session)
 
 
-# @pytest_asyncio.fixture
-# async def token(client, user) -> str:
-#     response = client.post(
-#         '/auth/login',
-#         data={'username': user.email, 'password': user.clean_password},
-#     )
+@pytest_asyncio.fixture
+async def token(user, session) -> str:
+    user.jti = uuid4()
+    session.add(user)
+    await session.commit()
+    return create_access_token({'sub': user.id, 'jti': user.jti}).jwt
 
-#     return response.headers['Authorization']
+
+@pytest.fixture
+def password():
+    return token_hex(4)
+
+
+@pytest_asyncio.fixture
+async def user(session, account, password):
+    user = await UserFactory.create(
+        session, account_id=account.id, pwd=password
+    )
+
+    return user
+
+
+@pytest_asyncio.fixture
+async def account(session) -> AccountFactory:
+    return await AccountFactory.create(session)
+
+
+@pytest_asyncio.fixture
+async def main_account(session):
+    return await AccountFactory()
+
+
+# @pytest_asyncio.fixture
+# async def main_user(session, main_account):
+#     user = await UserFactory.build(account_id=main_account.id)
 
 
 # @pytest_asyncio.fixture
