930bd4ca9762300a51ad9356c3a51a9c47951696
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 13:55:25 -0300

    Added imports

--- app/domain/accounts/schemas/__init__.py
+++ app/domain/accounts/schemas/__init__.py
@@ -1,3 +1,8 @@
 from .account import AccountCreate as AccountCreate
 from .account import AccountResponse as AccountResponse
 from .account import AccountUpdate as AccountUpdate
+from .auth import LoginRequest as LoginRequest
+from .auth import TokenResponse as TokenResponse
+from .user import UserCreate as UserCreate
+from .user import UserResponse as UserResponse
+from .user import UserUpdate as UserUpdate
