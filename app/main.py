0de84cef4b432bfc1ae070ce8e2eec27b7bfe49c
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 15:59:27 -0300

    Add logger configuration

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+import logging.config
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 import app.shared.registry
 from app.api.exceptions_handler import register_exception_handlers
 from app.api.router import router
+from config.logging import log_config
 from config.settings import settings
+
+logging.config.dictConfig(log_config)
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
