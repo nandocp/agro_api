9b00e49b86ad4b5f3c92b24237e684ec02c9b990
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 13:53:23 -0300

    Exchanged Session for SessionWithCommit

--- app/api/private/accounts.py
+++ app/api/private/accounts.py
@@ -5,13 +5,13 @@ from fastapi import APIRouter
 from app.domain.accounts.models import Account
 from app.domain.accounts.schemas import AccountCreate, AccountResponse
 from app.domain.accounts.services import AccountService
-from app.shared.dependencies import Session
+from app.shared.dependencies import SessionWithCommit
 
-router = APIRouter()
+router = APIRouter(tags=['accounts'])
 
 
 @router.post(
     '/', response_model=AccountResponse, status_code=HTTPStatus.CREATED
 )
-async def create_account(data: AccountCreate, session: Session):
+async def create_account(data: AccountCreate, session: SessionWithCommit):
     return await AccountService(Account, session).create_account(data)
