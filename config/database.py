8cfebc1586acf60cba42cd66c8bd3bb826dc639b
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 18:19:07 -0300

    Move get_session to shared.dependencies

--- config/database.py
+++ config/database.py
@@ -1,10 +1,5 @@
-from sqlalchemy.ext.asyncio import (
-    AsyncEngine,
-    AsyncSession,
-    create_async_engine,
-)
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
-from config.logging import logger
 from config.settings import settings
 
 engine: AsyncEngine = create_async_engine(
@@ -14,32 +9,3 @@ engine: AsyncEngine = create_async_engine(
     max_overflow=64,
     echo=settings.DEBUG,
 )
-
-
-def log_error(error: Exception):
-    logger.error(f'DB operation failed with {error}. Auto-rollbacking...')
-
-
-async def get_session():
-    async with AsyncSession(engine, expire_on_commit=False) as session:
-        try:
-            yield session
-        except Exception as error:
-            log_error(error)
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
-
-
-async def get_session_with_commit():
-    async with AsyncSession(engine, expire_on_commit=False) as session:
-        try:
-            yield session
-            await session.commit()
-        except Exception as error:
-            log_error(error)
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
