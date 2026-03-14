0d1107d1089379f44b97507fcfb663906f2e796b
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 18:24:31 -0300

    Update import

--- app/api/router.py
+++ app/api/router.py
@@ -6,7 +6,7 @@ from fastapi.responses import HTMLResponse
 # from app.api.public.router import router as public_router
 from app.api.auth.router import router as auth_router
 from app.api.private.router import router as private_router
-from config.authentication import get_current_user
+from app.shared.dependencies import get_current_user
 from config.settings import settings
 
 router = APIRouter()
