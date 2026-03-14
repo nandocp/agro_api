41de7a9524e4a6de7f1dad96557750b070ed416e
Fernando Caçador <fernando.cacador@proton.me>
2026-03-14 18:20:17 -0300

    Create /refresh_token route

--- app/api/auth/router.py
+++ app/api/auth/router.py
@@ -1,21 +1,33 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.domain.accounts.schemas import LoginRequest, TokenResponse
 from app.domain.accounts.services import UserService
-from app.shared.dependencies import SessionWithCommit
+from app.shared.dependencies import (
+    CurrentUser,
+    OAuth2Scheme,
+    SessionWithCommit,
+)
 
 router = APIRouter(tags=['auth'])
 
 
 @router.post('/login', response_model=TokenResponse)
 async def login(session: SessionWithCommit, form_data: LoginRequest):
-    service = await UserService(session).login(form_data)
-    return TokenResponse(access_token=service.token)
+    token = await UserService(session).login(form_data)
+    return TokenResponse(access_token=token)
+
+
+@router.post('/refresh_token', response_model=TokenResponse)
+async def refresh_token(
+    session: SessionWithCommit,
+    token: str = Depends(OAuth2Scheme),
+):
+    token = await UserService(session).refresh_token(token)
+    return TokenResponse(access_token=token)
 
 
 @router.delete('/logout', status_code=HTTPStatus.NO_CONTENT)
-async def logout(session: SessionWithCommit):
-    pass
-    # await UserService(Session).logout(user)
+async def logout(session: SessionWithCommit, current_user: CurrentUser):
+    await UserService(session).logout(current_user)
