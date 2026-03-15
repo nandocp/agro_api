6bc95ffb769e5034202180af454cac865dde1671
Fernando Caçador <fernando.cacador@proton.me>
2026-03-15 18:20:22 -0300

    Enabled plant discovery

--- app/domain/organisms/models/__init__.py
+++ app/domain/organisms/models/__init__.py
@@ -1,3 +1,4 @@
 from .common_name import OrganismCommonName as OrganismCommonName
+from .kingdom.plants import Plant as Plant
 from .organism import Organism as Organism
 from .synonym import OrganismSynonym as OrganismSynonym
