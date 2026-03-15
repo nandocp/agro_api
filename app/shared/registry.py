59a3afeed1602b91e6c743044cb1b8a12734da8f
Fernando Caçador <fernando.cacador@proton.me>
2026-03-15 15:20:30 -0300

    Add organism models to registry

--- app/shared/registry.py
+++ app/shared/registry.py
@@ -1,5 +1,6 @@
 import app.domain.accounts.models
 import app.domain.estates.models
 import app.domain.fields.models
+import app.domain.organisms.models
 import app.shared.address.model
 import app.shared.journal.entry
