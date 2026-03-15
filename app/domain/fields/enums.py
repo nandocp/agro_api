6708bc0dbe5003e3a4735b3eae8c29b4f30ebd21
Fernando Caçador <fernando.cacador@proton.me>
2026-03-15 08:32:36 -0300

    Added FieldProtectionKind and renamed FieldTransitionType to Kind

--- app/domain/fields/enums.py
+++ app/domain/fields/enums.py
@@ -1,6 +1,15 @@
 from enum import Enum
 
 
-class FieldTransitionType(str, Enum):
+class FieldTransitionKind(str, Enum):
     MERGE = 'merge'  # Multiple fields → One field
     SPLIT = 'split'  # One field → Multiple fields
+
+
+class FieldProtectionKind(str, Enum):
+    ENVIRONMENTAL = 'environmental'  # APP, APA, ARL, Reserva Legal
+    EMBARGO = 'embargo'  # IBAMA, órgão estadual
+    QUARANTINE = 'quarantine'  # fitossanitária
+    HERITAGE = 'heritage'  # histórico, arqueológico, indígena
+    CONTRACT = 'contract'  # arrendamento, comodato, penhor
+    EASEMENT = 'easement'  # servidão
