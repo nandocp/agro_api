926c0d5f9da42d7e388120b7b997385e43f3d80d
Fernando Caçador <fernando.cacador@proton.me>
2026-03-16 18:11:17 -0300

    Quiet script

--- scripts/toolbox/dbinit.sh
+++ scripts/toolbox/dbinit.sh
@@ -2,7 +2,7 @@
 
 # For now, to run DB, comment (or uncomment) the engine used
 # docker run --name agro_api_db -p 5432:5432 -dit -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres docker.io/postgis/postgis:18-3.6-alpine
-podman volume create --ignore agro_api_db
+podman volume create --ignore agro_api_db > /dev/null 2>&1
 podman run --name agro_api_db \
     -p 5432:5432 \
     -dit \
@@ -10,4 +10,5 @@ podman run --name agro_api_db \
     -e POSTGRES_USER=postgres \
     -e POSTGRES_PASSWORD=postgres \
     -v agro_api_db:/var/lib/postgresql \
-    docker.io/postgis/postgis:18-3.6-alpine "$@"
+    --replace \
+    docker.io/postgis/postgis:18-3.6-alpine "$@" > /dev/null 2>&1
