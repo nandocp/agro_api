8632f7c07ac081bfe501e580907d14be139fb89f
Fernando Caçador <fernando.cacador@proton.me>
2026-03-16 18:11:06 -0300

    Quiet script

--- scripts/toolbox/dbstop.sh
+++ scripts/toolbox/dbstop.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-podman stop --filter name=agro_api_db
-podman rm --filter name=agro_api_db
+podman stop --filter name=agro_api_db > /dev/null 2>&1
+podman rm --filter name=agro_api_db > /dev/null 2>&1
