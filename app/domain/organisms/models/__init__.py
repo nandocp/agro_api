195cf09e9ab90746277678f526637cf82aaad7a5
Fernando Caçador <fernando.cacador@proton.me>
2026-03-16 18:42:23 -0300

    Enabled organism traits discovery

--- app/domain/organisms/models/__init__.py
+++ app/domain/organisms/models/__init__.py
@@ -2,3 +2,4 @@ from .common_name import OrganismCommonName as OrganismCommonName
 from .kingdom.plants import Plant as Plant
 from .organism import Organism as Organism
 from .synonym import OrganismSynonym as OrganismSynonym
+from .trait import OrganismTrait as OrganismTrait
