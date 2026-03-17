0546e6513e7dc36d7e3cd6eaf9d96a5751fcb093
Fernando Caçador <fernando.cacador@proton.me>
2026-03-16 22:29:47 -0300

    Enabled SoilClassification and FieldSoilClassification discovery

--- app/domain/fields/models/__init__.py
+++ app/domain/fields/models/__init__.py
@@ -1,4 +1,8 @@
 from .field import Field as Field
 from .protection import FieldProtection as FieldProtection
 from .soil_analysis import FieldSoilAnalysis as FieldSoilAnalysis
+from .soil_classification import (
+    FieldSoilClassification as FieldSoilClassification,
+)
+from .soil_classification import SoilClassification as SoilClassification
 from .transition import FieldTransition as FieldTransition
