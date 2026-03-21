5c7f4c78e88da8627b778fe6dcfe4678a18a290a
Fernando Caçador <fernando.cacador@proton.me>
2026-03-20 23:06:23 -0300

    Created classify_slope

--- tests/shared/test_utils.py
+++ tests/shared/test_utils.py
@@ -1,4 +1,9 @@
-from app.shared.utils import slugify
+from app.shared.utils import (
+    MaxSlopePercent,
+    SlopeClass,
+    classify_slope,
+    slugify,
+)
 
 
 def test_slugify_basic():
@@ -31,3 +36,29 @@ def test_slugify_leading_trailing_spaces():
 
 def test_slugify_leading_trailing_hiphens():
     assert slugify('-Fazenda Teste-') == 'fazenda-teste'
+
+
+def test_classify_flat_slope():
+    assert classify_slope(MaxSlopePercent.FLAT - 1) == SlopeClass.FLAT
+
+
+def test_classify_gentle_slope():
+    assert classify_slope(MaxSlopePercent.GENTLE - 1) == SlopeClass.GENTLE
+
+
+def test_classify_moderate_slope():
+    assert classify_slope(MaxSlopePercent.MODERATE - 1) == SlopeClass.MODERATE
+
+
+def test_classify_strong_slope():
+    assert classify_slope(MaxSlopePercent.STRONG - 1) == SlopeClass.STRONG
+
+
+def test_classify_steep_slope():
+    assert classify_slope(MaxSlopePercent.STEEP - 1) == SlopeClass.STEEP
+
+
+def test_classify_very_steep_slope():
+    assert (
+        classify_slope(MaxSlopePercent.VERY_STEEP - 1) == SlopeClass.VERY_STEEP
+    )
