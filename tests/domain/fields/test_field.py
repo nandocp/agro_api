b5ae51445cb9d474584f31e8b80a4573d433611d
Fernando Caçador <fernando.cacador@proton.me>
2026-03-20 22:57:57 -0300

    Added properties tests

--- tests/domain/fields/test_field.py
+++ tests/domain/fields/test_field.py
@@ -1,12 +1,29 @@
-# import pytest
+from datetime import datetime
+
+import pytest
+
+from tests.factories.fields import FieldFactory
 
 
-# @pytest.mark.asyncio
-# async def test_field_repr_(field):
-#     _repr = (
-#         f'Field(id={field.id}, '
-#         f'slug={field.slug}, '
-#         f'estate={field.estate.slug}, '
-#         f'created_at={field.created_at})'
-#     )
-#     assert str(field) == _repr
+@pytest.mark.asyncio
+async def test_field_repr_(field):
+    _repr = (
+        f'Field(id={field.id}, '
+        f'slug={field.slug}, '
+        f'estate={field.estate_id}, '
+        f'created_at={field.created_at})'
+    )
+    assert str(field) == _repr
+
+
+# is_active
+async def test_is_active():
+    field = await FieldFactory.build()
+    field.active_to = None
+    assert field.is_active
+
+
+async def test_is_not_active():
+    field = await FieldFactory.build()
+    field.active_to = datetime.now()
+    assert not field.is_active
