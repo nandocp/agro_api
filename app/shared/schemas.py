804fb5c664d7c783d899fc835b1b63e1350f46c9
Fernando Caçador <fernando.cacador@proton.me>
2026-03-22 13:33:37 -0300

    Created paginationr response schema

--- app/shared/schemas.py
+++ app/shared/schemas.py
@@ -1,11 +1,23 @@
 from datetime import datetime
+from typing import Generic, TypeVar
 
 from pydantic import UUID7, BaseModel, Field
+
+T = TypeVar('T')
 
 
 class Pagination(BaseModel):
     offset: int | None = Field(ge=0, default=0)
     limit: int | None = Field(ge=0, default=10)
+
+
+class PaginatedResponse(BaseModel, Generic[T]):
+    data: list[T]
+    total: int
+    offset: int
+    limit: int
+    has_next: bool
+    has_previous: bool
 
 
 class BaseSchema(BaseModel):
