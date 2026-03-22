8c06d279f12fcbd63da5cad80294fc5f359cdee1
Fernando Caçador <fernando.cacador@proton.me>
2026-03-21 22:58:58 -0300

    Refactored service; Created #create, #update_plan, #show

--- app/domain/accounts/services/account.py
+++ app/domain/accounts/services/account.py
@@ -1,8 +1,36 @@
+from uuid import UUID
+
 from app.domain.accounts.models import Account
-from app.domain.accounts.schemas import AccountCreate, AccountUpdate
+from app.domain.accounts.repositories import AccountRepository
+from app.domain.accounts.schemas import AccountCreate, AccountUpdatePlan
+from app.shared.exceptions import NotFoundError
 from app.shared.service import BaseService
 
 
-class AccountService(BaseService[Account, AccountCreate, AccountUpdate]):
-    async def create_account(self, object: AccountCreate):
-        return await self.repo.create(object)
+class AccountService(BaseService):
+    def __init__(self, session):
+        super().__init__(session)
+        self.repo = AccountRepository(session)
+
+    async def create(self, account_create: AccountCreate):
+        return await self.repo.create(account_create)
+
+    async def update_plan(
+        self,
+        plan_update: AccountUpdatePlan,
+        account_id: UUID,
+    ):
+        account = await self.repo.get_one(account_id)
+        if not account:
+            raise NotFoundError('account')
+
+        return await self.repo.update(
+            account, {'plan': plan_update.plan.value}
+        )
+
+    async def show(self, account_id: UUID) -> Account:
+        account = await self.repo.get_with_relations(account_id)
+        if not account:
+            raise NotFoundError('account')
+
+        return account
