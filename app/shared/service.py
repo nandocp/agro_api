bcd99813d3beeb076fefc890668cee188aa89bd5
Fernando Caçador <fernando.cacador@proton.me>
2026-03-21 22:54:15 -0300

    Refactored BaseService to decrease coupling

--- app/shared/service.py
+++ app/shared/service.py
@@ -1,18 +1,6 @@
-from abc import ABC
-from typing import Generic, Type, TypeVar
-
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.shared.crud import CRUDBase
 
-ModelType = TypeVar('ModelType')
-CreateSchemaType = TypeVar('CreateSchemaType')
-UpdateSchemaType = TypeVar('UpdateSchemaType')
-
-
-class BaseService(ABC, Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
-    def __init__(self, model: Type[ModelType], session: AsyncSession) -> None:
+class BaseService:
+    def __init__(self, session: AsyncSession) -> None:
         self.session = session
-        self.repo = CRUDBase[ModelType, CreateSchemaType, UpdateSchemaType](
-            model, session
-        )
