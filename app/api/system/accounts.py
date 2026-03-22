f53530c16742a402d0918156623c511775f67700
Fernando Caçador <fernando.cacador@proton.me>
2026-03-22 13:36:37 -0300

    Created list_accounts method; Renamed some others

--- app/api/system/accounts.py
+++ app/api/system/accounts.py
@@ -1,18 +1,39 @@
 from http import HTTPStatus
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from app.domain.accounts.schemas import AccountCreate, AccountUpdatePlan
+from app.domain.accounts.schemas import (
+    AccountCreate,
+    AccountFilters,
+    AccountUpdatePlan,
+)
 from app.domain.accounts.schemas import AccountResponse as Response
 from app.domain.accounts.services import AccountService
 from app.shared.dependencies import SessionWithCommit
+from app.shared.schemas import PaginatedResponse
 
 router = APIRouter(tags=['accounts'])
 
 
+@router.get(
+    '', response_model=PaginatedResponse[Response], status_code=HTTPStatus.OK
+)
+async def list_accounts(
+    session: SessionWithCommit, filters: AccountFilters = Depends()
+):
+    return await AccountService(session).index(filters)
+
+
+@router.get(
+    '/{account_id}', response_model=Response, status_code=HTTPStatus.OK
+)
+async def show_account(account_id, session: SessionWithCommit):
+    return await AccountService(session).show(account_id)
+
+
 @router.post('', response_model=Response, status_code=HTTPStatus.CREATED)
-async def create(data: AccountCreate, session: SessionWithCommit):
+async def create_account(data: AccountCreate, session: SessionWithCommit):
     return await AccountService(session).create(data)
 
 
@@ -23,10 +44,3 @@ async def update_plan(
     data: AccountUpdatePlan, session: SessionWithCommit, account_id: UUID
 ):
     return await AccountService(session).update_plan(data, account_id)
-
-
-@router.get(
-    '/{account_id}', response_model=Response, status_code=HTTPStatus.OK
-)
-async def show(account_id, session: SessionWithCommit):
-    return await AccountService(session).show(account_id)
