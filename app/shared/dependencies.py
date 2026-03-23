9fb2942b2ebfe361236616b5cf7c9dc12d93b256
Fernando Caçador <fernando.cacador@proton.me>
2026-03-23 17:36:00 -0300

    Update

--- app/shared/dependencies.py
+++ app/shared/dependencies.py
@@ -1,14 +1,15 @@
-from typing import Annotated, Type, TypeVar
+from typing import Annotated, TypeVar
 
-from fastapi import Depends, Query
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from jwt import DecodeError
 from pydantic import BaseModel
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
-from app.domain.accounts.auth import decode_access_token
+from app.domain.accounts.auth import (
+    decode_and_validate_token,
+)
 from app.domain.accounts.models import Role, User
 from app.shared.exceptions import InvalidCredentialsError
 from config.database import engine
@@ -17,11 +18,11 @@ from config.logging import logger
 FilterSchemaType = TypeVar('FilterSchemaType', bound=BaseModel)
 
 
-def log_error(error: Exception):
+def log_error(error: Exception):  # pragma: no cover
     logger.error(f'DB operation failed with {error}. Auto-rollbacking...')
 
 
-async def get_session():
+async def get_session():  # pragma: no cover
     async with AsyncSession(engine, expire_on_commit=False) as session:
         try:
             yield session
@@ -33,7 +34,7 @@ async def get_session():
             await session.close()
 
 
-async def get_session_with_commit():
+async def get_session_with_commit():  # pragma: no cover
     async with AsyncSession(engine, expire_on_commit=False) as session:
         try:
             yield session
@@ -53,15 +54,7 @@ async def get_current_user(
     session: AsyncSession = Depends(get_session),
     token: str = Depends(OAuth2Scheme),
 ) -> User:
-    try:
-        payload = decode_access_token(token)
-        jti = payload.get('jti')
-        sub = payload.get('sub')
-
-        if not sub or not jti:
-            raise InvalidCredentialsError
-    except DecodeError:
-        raise InvalidCredentialsError
+    sub, jti = decode_and_validate_token(token)
 
     user = await session.scalar(
         select(User)
@@ -76,10 +69,6 @@ async def get_current_user(
         raise InvalidCredentialsError
 
     return user
-
-
-def filters(filter_schema: Type[FilterSchemaType]):
-    return Annotated[filter_schema, Query()]
 
 
 Session = Annotated[AsyncSession, Depends(get_session)]
