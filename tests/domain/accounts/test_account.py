4cb49baa82989b1835aa4a20e787b973173a4cba
Fernando Caçador <fernando.cacador@proton.me>
2026-03-30 19:10:08 -0300

    File rename

--- tests/domain/accounts/test_account.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from tests.factories.accounts import AccountFactory
-
-
-@pytest.mark.asyncio
-async def test_account_repr_(session):
-    account = await AccountFactory.create(session)
-    assert str(account) == f'Account(id={account.id}, name={account.name})'
